97431f67df8a6ca9c574e6c0605b7a45513d5090
Manguberdi78 <muhammadxudayberganov785@gmail.com>
2023-02-03 18:50:43 +0500

    mangu

---
+++ services.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+<?php include 'vendor/head.php'; ?>
+    <title> sevices </title>
+</head>
+<body>
+<?
+include "vendor/navbar.php";
+?>
+<main id="main">
+<?php include 'vendor/foother.php';
+?>
+</body>
+</html>
---
+++ vendor/navbar.php
@@ -0,0 +1,24 @@
+<header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center">
+
+        <h1 class="logo me-auto"><a href="index.html">Sailor</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+        <nav id="navbar" class="navbar">
+            <ul>
+                <li><a href="../index.php" class="active">Home</a></li>
+                </li>
+                <li><a href="../services.php">Services</a></li>
+                <li><a href="../portfolio.html">Portfolio</a></li>
+                <li><a href="../pricing.html">Pricing</a></li>
+                <li><a href="../blog.html">Blog</a></li>
+
+                <li><a href="../contact.html">Contact</a></li>
+                <li><a href="../index.html" class="getstarted">Get Started</a></li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav><!-- .navbar -->
+
+    </div>
+</header>
